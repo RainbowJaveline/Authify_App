01c793b17db44a3e2d0c7af77092eefa8b3ffa40
RainbowJaveline <nealmudaliar70@gmail.com>
2026-07-06 23:38:09 +0530

    added the Endpoints for , is-authenticated , send-resetPassword and reset-Password

--- src/main/java/Authentication/Project1/Authify/Controller/AuthController.java
+++ src/main/java/Authentication/Project1/Authify/Controller/AuthController.java
@@ -1,10 +1,14 @@
 package Authentication.Project1.Authify.Controller;
 
+import Authentication.Project1.Authify.Repository.UserRepository;
 import Authentication.Project1.Authify.Service.AppUserDetailsService;
+import Authentication.Project1.Authify.Service.ProfileImpl;
 import Authentication.Project1.Authify.Util.JwtUtil;
 import Authentication.Project1.Authify.io.AuthRequest;
 import Authentication.Project1.Authify.io.AuthResponse;
+import Authentication.Project1.Authify.io.ResetPasswordRequest;
 import io.jsonwebtoken.Jwt;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -14,10 +18,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.Duration;
 import java.util.HashMap;
@@ -30,6 +34,8 @@ public class AuthController {
     private final AuthenticationManager manager;
     private final AppUserDetailsService userDetailsService;
     private final JwtUtil jwtUtil;
+    private final ProfileImpl profile;
+    private final UserRepository userRepository;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthRequest authRequest){
@@ -67,6 +73,31 @@ public class AuthController {
     private void authenticate(String email, String password) {
         manager.authenticate(new UsernamePasswordAuthenticationToken(email,password));
     }
+
+    @GetMapping("/is-authenticated")
+    public ResponseEntity<Boolean> isAuthenticated(@CurrentSecurityContext(expression = "authentication?.name") String email){
+        return ResponseEntity.ok(email != null);
+    }
+
+    @PostMapping("/send-resetPassword-otp")
+    public void resetPassword(@RequestParam String email){
+        try {
+            profile.sendResetOtp(email);
+        }catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR , e.getMessage());
+        }
+    }
+
+    @PostMapping("/reset-Password")
+    public void resetPassword(@Valid @RequestBody ResetPasswordRequest passwordRequest){
+        try{
+            profile.resetPassword(passwordRequest.getEmail() , passwordRequest.getOtp(), passwordRequest.getNewPassword());
+        }catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR , e.getMessage());
+        }
+    }
+
+
 
 
 }
